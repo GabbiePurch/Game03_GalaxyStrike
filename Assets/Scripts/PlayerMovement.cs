998c331f2aa7e4b27017263b79864333ae9eaa30
GabbiePurch <purchi375@morrisville.edu>
2025-10-22 16:27:04 -0400

    Player Movement Implemented with position limits

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,20 +3,36 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+   [SerializeField] float controlSpeed = 10f;
+   [SerializeField] float xClampRange = 5f;
+   [SerializeField] float yClampRange = 5f;
 
-    // Update is called once per frame
+   Vector2 movement;
+
     void Update()
     {
-        
+        ProcessTranslation();
     }
+        
+
 
     public void OnMove(InputValue value)
     {
-        Debug.Log(value.Get<Vector2>());
+        movement = value.Get<Vector2>();
     }
+
+    private void ProcessTranslation()
+    {
+        float xOffset = movement.x * controlSpeed * Time.deltaTime;
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xClampRange, xClampRange);
+
+
+        float yOffset = movement.y * controlSpeed * Time.deltaTime;
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, -yClampRange, yClampRange);
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos ,0f);
+    }
+    
 }
