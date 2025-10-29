f19d0ce9f6449a3cc16f715cd8a45c03000f623d
GabbiePurch <purchi375@morrisville.edu>
2025-10-29 15:18:35 -0400

    Crosshair UI added and Aligned with mouse position

--- Assets/Scripts/PlayerWeapon.cs
+++ Assets/Scripts/PlayerWeapon.cs
@@ -4,12 +4,19 @@ using UnityEngine.UIElements.Experimental;
 
 public class PlayerWeapon : MonoBehaviour
 {
-    [SerializeField] GameObject laser;
+    [SerializeField] GameObject[] lasers;
+    [SerializeField] RectTransform crosshair;
     bool isFiring = false;
+
+    void Start()
+    {
+        Cursor.visible = false;
+    }
 
     private void Update()
     {
         ProcessFiring();
+        MoveCrosshair();
     }
     public void OnFire(InputValue value)
     {
@@ -18,7 +25,17 @@ public class PlayerWeapon : MonoBehaviour
 
     void ProcessFiring()
     {
-        var emmissionModule = laser.GetComponent<ParticleSystem>().emission;
-        emmissionModule.enabled = isFiring;
+        foreach (GameObject laser in lasers)
+        {
+            var emmissionModule = laser.GetComponent<ParticleSystem>().emission;
+            emmissionModule.enabled = isFiring;
+
+        }
+    }
+    
+
+    void MoveCrosshair()
+    {
+        crosshair.position = Input.mousePosition;
     }
 }
