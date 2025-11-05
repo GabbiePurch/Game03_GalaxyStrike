359fd679417aeebce694febbfa71cc5e3dcda483
GabbiePurch <purchi375@morrisville.edu>
2025-11-05 11:04:05 -0500

    Creative gameplay feature added to Galaxy Strike.
    Added  a Shield to mu game

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,12 +1,39 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CollisionHandler : MonoBehaviour
 {
     [SerializeField] GameObject destroyVFX;
+    bool shieldActive = false;
+    float shieldTimer = 0f;
+
+    private void Update()
+    {
+        if (shieldActive)
+        {
+            shieldTimer -= Time.deltaTime;
+
+            if (shieldTimer <= 0f)
+            {
+                shieldActive = false;
+            }
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
+        if (other.gameObject.CompareTag("Shield"))
+        {
+            shieldActive = true;
+            shieldTimer = 5f;
+
+            Destroy(other.gameObject);
+            return;
+        }
+
+        if (shieldActive) return;
         Instantiate(destroyVFX, transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
+
 }
