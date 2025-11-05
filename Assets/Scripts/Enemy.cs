57ef2e4bce9dd0254c048865fa0ddee7dddfde62
GabbiePurch <purchi375@morrisville.edu>
2025-11-05 14:36:11 -0500

    Individual enemy hit points implemented

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,9 +3,23 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject destroyVFX;
+    [SerializeField] int hitPoints = 3;
+
     private void OnParticleCollision(GameObject other)
     {
-        Instantiate(destroyVFX, transform.position, Quaternion.identity);
-        Destroy(this.gameObject);
+        ProcessHit();
+    }
+
+    private void ProcessHit()
+    {
+        hitPoints--;
+
+        if (hitPoints <= 0)
+        {
+
+            Instantiate(destroyVFX, transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+
+        }
     }
 }
